8f789954fc655876ccd392502f07c04fede0a0ef
SatakeYusuke19920527 <satkyusk@icloud.com>
2021-01-18 23:23:14 +0900

    資料完成

--- src/components/PageA.tsx
+++ src/components/PageA.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import response from '../types/response.json';
 
-const PageA = () => {
+type ResponseType = typeof response;
+
+const PageA: React.FC<{}> = () => {
+  const [data, setData] = useState<ResponseType[]>([]);
+  const handleFetch = async () => {
+    const result = await axios.get(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+    // eslint-disable-next-line
+    const tempArray = result.data.filter((d: ResponseType) => {
+      if (d.id % 2) {
+        return d;
+      }
+    });
+    setData(tempArray);
+  };
   return (
     <div>
-      <h1>PageA</h1>
+      <button onClick={handleFetch}>fetch</button>
+      {data &&
+        data.map((element, index) => {
+          return (
+            <div key={index}>
+              {element.id}/{element.name}
+            </div>
+          );
+        })}
     </div>
   );
 };
